8ba399cf658aeba732fc431930e2db61cdb16127
repodeep <129058395+repodeep@users.noreply.github.com>
2024-04-30 19:12:58 +0530

    Time: 10 ms (66.44%) | Memory: 13.7 MB (44.22%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Brute force
+        // // Brute force
+        // int n = nums.size();
+        // for(int i=0; i<n; i++) {
+        //     // for(int j=0; j<n; j++) {
+        //     for(int j=i+1; j<n; j++) {
+        //         // if(i==j) {
+        //         //     continue;
+        //         // }
+        //         if(nums[i] + nums[j] == target) {
+        //             return {min(i,j), max(i,j)};
+        //         }
+        //     }
+        // }
+        // return {-1, -1};
+
+
+        // Better Solution
         int n = nums.size();
+        map<int, int> mpp;
+
         for(int i=0; i<n; i++) {
-            // for(int j=0; j<n; j++) {
-            for(int j=i+1; j<n; j++) {
-                // if(i==j) {
-                //     continue;
-                // }
-                if(nums[i] + nums[j] == target) {
-                    return {min(i,j), max(i,j)};
-                }
+            int a = nums[i];
+            int more = target - a;
+
+            if(mpp.find(more) != mpp.end()) {
+                // return "Yes"; // variety 1 of this problem(discussed by striver)
+                return {mpp[more], i}; // mpp[more] < i (since it's presence in the map ensures that it was already encountered in the array 'before(meaning at some idx < i)' the current ith element)
             }
+
+            // mpp[a] = i;
+            mpp.insert(make_pair(a, i));
+            // mpp.insert({a, i});
         }
+
         return {-1, -1};
     }
 };
