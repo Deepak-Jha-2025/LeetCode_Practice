673c9aa180263dec00ccc8c31053a8316678e95f
repodeep <129058395+repodeep@users.noreply.github.com>
2024-05-19 19:32:58 +0530

    Time: 20 ms (39.48%) | Memory: 26.5 MB (91.15%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,33 +1,74 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Better solution
+        // // Better solution 
+        // // T.C = O(N*logN) + O(N)
+        // // S.C = O(N)
 
-        // map to store the count
-        unordered_map<int, int> mpp;
+        // // map to store the count
+        // map<int, int> mpp;
 
-        // Iterate in the array and store the counts --> O(N*log(N))
+        // // Iterate in the array and store the counts --> O(N*log(N))
+        // int n = nums.size();
+        // for(int i=0; i<n; i++) {
+        //     mpp[nums[i]]++;
+
+        //     // auto it = mpp.find(nums[i]);
+        //     // if(it == mpp.end()) {
+        //     //     mpp[nums[i]] = 1;
+        //     //     // mpp.insert({nums[i], 1});
+        //     //     // mpp[nums[i]] = mpp[nums[i]] + 1; // This handles both if-else cases automatically
+        //     // }
+        //     // else {
+        //     //     mpp[nums[i]]++;
+        //     // }
+        // }
+
+        // // Iterate in the map and find the majority element --> O(N) (worst case if all elements unique)
+        // for(auto pr : mpp) {
+        //     if(pr.second > n/2) {
+        //         return pr.first;
+        //     }
+        // }
+        // return -1;
+
+
+
+        // Optimal Solution (Moore's Voting Algorithm)
+
+        // Take the 2 variables, el and cnt
+        int el;
+        int cnt = 0;
+
+        // Iterate and apply the MVE
         int n = nums.size();
         for(int i=0; i<n; i++) {
-            mpp[nums[i]]++;
-
-            // auto it = mpp.find(nums[i]);
-            // if(it == mpp.end()) {
-            //     mpp[nums[i]] = 1;
-            //     // mpp.insert({nums[i], 1});
-            //     // mpp[nums[i]] = mpp[nums[i]] + 1; // This handles both if-else cases automatically
-            // }
-            // else {
-            //     mpp[nums[i]]++;
-            // }
-        }
-
-        // Iterate in the map and find the majority element
-        for(auto pr : mpp) {
-            if(pr.second > n/2) {
-                return pr.first;
+            // Start with a check for a new section/part/array
+            if(cnt == 0) {
+                el = nums[i];
+                cnt++;
+            }
+            // else if cnt != 0 but curr. element = el
+            else if(nums[i] == el) {
+                cnt++;
+            }
+            // else if cnt != 0 but curr. element != el
+            else {
+                cnt--;
             }
         }
-        return -1;
+
+        // Iterate again and check if the obtained excess element in el == majority element
+        int cnt1 = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == el) {
+                cnt1++;
+            }
+        }
+
+        if(cnt1 > n/2) {
+            return el;
+        }
+        return -1; // majority element does not exist
     }
 };
