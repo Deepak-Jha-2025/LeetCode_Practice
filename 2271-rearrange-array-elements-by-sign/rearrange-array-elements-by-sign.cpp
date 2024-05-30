f99a8b4384a38f08fae53c2b0d2b3f08f485722e
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-05-30 13:09:42 +0530

    Time: 159 ms (10.58%) | Memory: 137 MB (24.75%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -61,60 +61,80 @@ public:
             }
         }
 
-        // Possible 2 cases
-        if(pos.size() > neg.size()) {
-            // Iterate for as many negatives are there, same no. of positives will also be taken
-            for(int i=0; i<neg.size(); i++) {
-                nums[2*i] = pos[i];
-                nums[2*i+1] = neg[i];
-
-                // This makes sure that for eg: if 2 negatives are there, 4 elemnts are done
-                // (i.e. twice the no. of neg elements or 2*neg.size()) 
-                // 2 neg and 2 pos, basically all the neg are done
-            }
-
-            // Now, for the remaining pos no.s
-            
-            // No. of left pos no.s  = pos.size() - neg.size()
-            // No. of taken pos no.s = as many neg no.s are there = neg.size() = 2
-            // Reamining pos no.s will start from index = neg.size() = 2
-
-            // For arr[], 4 elements filled = twice the no. of neg elements = 2*neg.size()
-            int idx = neg.size() * 2;
-            for(int i=neg.size(); i<pos.size(); i++) {
-                nums[idx] = pos[i];
-                idx++;
-            } 
+        // Shortcut combination of 2 cases described below
+        int minSize = min(pos.size(), neg.size());
+        int maxSize = max(pos.size(), neg.size());
+        for(int i=0; i<minSize; i++) {
+            nums[2*i] = pos[i];
+            nums[2*i+1] = neg[i];
         }
-        else {
-            // This will also cover the case if pos.size() == neg.size()
 
-            // Here, everything is same, just neg are in excess and pos are less
-            // So, just exchange neg and pos
-
-            // Iterate for as many positives are there, same no. of negatives will also be taken
-            for(int i=0; i<pos.size(); i++) {
-                nums[2*i] = pos[i];
-                nums[2*i+1] = neg[i];
-
-                // This makes sure that for eg: if 2 pos are there, 4 elemnts are done
-                // (i.e. twice the no. of pos elements or 2*pos.size()) 
-                // 2 neg and 2 pos, basically all the neg are done
+        int index = 2 * minSize;
+        for(int i=minSize; i<maxSize; i++) {
+            if(maxSize = pos.size()) {
+                nums[index] = pos[i];
+                index++;
             }
-
-            // Now, for the remaining neg no.s
-            
-            // No. of left neg no.s  = neg.size() - pos.size()
-            // No. of taken neg no.s = as many pos no.s are there = pos.size() = 2
-            // Reamining neg no.s will start from index = pos.size() = 2
-
-            // For arr[], 4 elements filled = twice the no. of pos elements = 2*pos.size()
-            int idx = pos.size() * 2;
-            for(int i=pos.size(); i<neg.size(); i++) {
-                nums[idx] = neg[i];
-                idx++;
+            else {
+                nums[index] = neg[i];
+                index++;
             }
         }
+
+        // // Possible 2 cases
+        // if(pos.size() > neg.size()) {
+        //     // Iterate for as many negatives are there, same no. of positives will also be taken
+        //     for(int i=0; i<neg.size(); i++) {
+        //         nums[2*i] = pos[i];
+        //         nums[2*i+1] = neg[i];
+
+        //         // This makes sure that for eg: if 2 negatives are there, 4 elemnts are done
+        //         // (i.e. twice the no. of neg elements or 2*neg.size()) 
+        //         // 2 neg and 2 pos, basically all the neg are done
+        //     }
+
+        //     // Now, for the remaining pos no.s
+            
+        //     // No. of left pos no.s  = pos.size() - neg.size()
+        //     // No. of taken pos no.s = as many neg no.s are there = neg.size() = 2
+        //     // Reamining pos no.s will start from index = neg.size() = 2
+
+        //     // For arr[], 4 elements filled = twice the no. of neg elements = 2*neg.size()
+        //     int idx = neg.size() * 2;
+        //     for(int i=neg.size(); i<pos.size(); i++) {
+        //         nums[idx] = pos[i];
+        //         idx++;
+        //     } 
+        // }
+        // else {
+        //     // This will also cover the case if pos.size() == neg.size()
+
+        //     // Here, everything is same, just neg are in excess and pos are less
+        //     // So, just exchange neg and pos
+
+        //     // Iterate for as many positives are there, same no. of negatives will also be taken
+        //     for(int i=0; i<pos.size(); i++) {
+        //         nums[2*i] = pos[i];
+        //         nums[2*i+1] = neg[i];
+
+        //         // This makes sure that for eg: if 2 pos are there, 4 elemnts are done
+        //         // (i.e. twice the no. of pos elements or 2*pos.size()) 
+        //         // 2 neg and 2 pos, basically all the neg are done
+        //     }
+
+        //     // Now, for the remaining neg no.s
+            
+        //     // No. of left neg no.s  = neg.size() - pos.size()
+        //     // No. of taken neg no.s = as many pos no.s are there = pos.size() = 2
+        //     // Reamining neg no.s will start from index = pos.size() = 2
+
+        //     // For arr[], 4 elements filled = twice the no. of pos elements = 2*pos.size()
+        //     int idx = pos.size() * 2;
+        //     for(int i=pos.size(); i<neg.size(); i++) {
+        //         nums[idx] = neg[i];
+        //         idx++;
+        //     }
+        // }
 
         return nums;
     }
