a9a73dc83230cfc0cb79effcd0a7ab83839c7cb5
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-10 18:01:41 +0530

    Time: 67 ms (66.12%) | Memory: 50.6 MB (29.53%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -17,9 +17,16 @@ public:
             preSum += nums[i];
             int removeSum = preSum - k;
 
-            if(preSumMap.find(removeSum) != preSumMap.end()) {
-                cnt += preSumMap[removeSum];
-            }
+
+            // if(preSumMap.find(removeSum) != preSumMap.end()) {
+            //     cnt += preSumMap[removeSum];
+            // }
+
+            cnt += preSumMap[removeSum]; // directly writing also works but it unnecessarily will store
+                                          // the removeSum in the map with a count = 0 (which will stay 
+                                          // unaltered afterwards)
+
+
 
             // auto it = preSumMap.find(preSum);
             // if(it == preSumMap.end()) {
