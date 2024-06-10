0a39e4f573a8667cc1c0ea1d55a9257cf7ae76de
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-10 17:55:16 +0530

    Time: 54 ms (92.82%) | Memory: 45.1 MB (59.09%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
-        mpp[0] = 1;
+        // Optimal solution
+        
+        unordered_map<int, int> preSumMap;
 
-        int preSum = 0, cnt = 0;
+        int preSum = 0;
+        int cnt = 0;
+
+        // inserting (0,1)
+        // preSumMap.insert({0,1});
+        preSumMap[0] = 1;
+
         int n = nums.size();
-
         for(int i=0; i<n; i++) {
             preSum += nums[i];
-            int remove = preSum - k;
-            cnt += mpp[remove];
-            mpp[preSum] += 1;
+            int removeSum = preSum - k;
+
+            if(preSumMap.find(removeSum) != preSumMap.end()) {
+                cnt += preSumMap[removeSum];
+            }
+
+            // auto it = preSumMap.find(preSum);
+            // if(it == preSumMap.end()) {
+            //     // occurs 1st time
+            //     preSumMap.insert({preSum, 1});
+            // }
+            // else {
+            //     // already exists, so just increment the count
+            //     // it->second++;
+            //     preSumMap[preSum]++;
+            // }
+
+            // Shortcut statement for above
+            preSumMap[preSum] = preSumMap[preSum] + 1; // directly inserts 1, i.e. preSum occurs the 1st time if not present
+                                                       // else if already present, then just increments the count
         }
 
         return cnt;
