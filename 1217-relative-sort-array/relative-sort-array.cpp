145ab58bd537bdf63bf2bbcdd0246ebc5ce55b44
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-11 16:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (24.90%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        // Brute Force
-        vector<int> arr3;
-        vector<int> leftOverElements;
+        // Better Solution
+        map<int, int> mpp;
 
         int m = arr1.size();
         int n = arr2.size();
 
-        for(int i=0; i<n; i++) {
-            int toFind = arr2[i];
-
-            for(int j=0; j<m; j++) {
-                if(arr1[j] == toFind) {
-                    arr3.push_back(arr1[j]);
-                    arr1[j] = -1;
-                }
-            }
-        }
-
         for(int i=0; i<m; i++) {
-            // Since we marked all elements taken as -ve, whatever is +ve are the leftovers
-            if(arr1[i] >= 0) {
-                leftOverElements.push_back(arr1[i]);
+            mpp[arr1[i]]++;
+        }
+
+        int cnt=0, temp = 0;
+        for(int i=0; i<n; i++) {
+            temp = 0;
+            while(temp < mpp[arr2[i]]) {
+                arr1[cnt] = arr2[i];
+                temp++, cnt++;
+            }
+
+            mpp.erase(arr2[i]);
+        }
+
+        for(auto it : mpp) {
+            temp = 0;
+            while(temp < it.second) {
+                arr1[cnt] = it.first;
+                temp++, cnt++;
             }
         }
 
-        sort(leftOverElements.begin(), leftOverElements.end());
-
-        for(int i=0; i<leftOverElements.size(); i++) {
-            arr3.push_back(leftOverElements[i]);
-        }
-
-        return arr3;
+        return arr1;
     }
 };
