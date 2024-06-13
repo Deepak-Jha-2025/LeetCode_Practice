a98b7520e826fa9f896a0919733eb0632bc7b6eb
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-13 14:12:07 +0530

    Time: 8 ms (39.38%) | Memory: 21 MB (52.06%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        // Better Solution
+        // Optimal Solution (counting sort, only when constraints are small, like elements in both arrays <= 100)
 
-        // Intuition: rightmost student should take the rightmost seat and leftmost student should take the leftmost
-        // seat, as logically, it will be respectively the closest to them. Then subsequently, the 2nd leftmost and 
-        // the 2nd rightmost student should take the 2nd leftmost and 2nd right most seat respectively, and so on.
+        int maxPos = 0;
+        maxPos = max(seats[0], students[0]);
 
-        // Basically if seat positions and student positions are sorted, then optimally the nearest seat for ith
-        // student will be ith seat.
-
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
-
-        int cnt = 0;
         int n = seats.size();
-        for(int i=0; i<n; i++) {
-            cnt += abs(students[i] - seats[i]);
+        for(int i=1; i<n; i++) {
+            maxPos = max(maxPos, max(seats[i], students[i]));
         }
 
-        return cnt;
+        int m = maxPos;
+        vector<int> differences(m+1, 0);
+
+        for(int i=0; i<n; i++) {
+            differences[seats[i]]++;
+        }
+
+        for(int i=0; i<n; i++) {
+            differences[students[i]]--;
+        }
+
+        // differences[i] == 0  ==> empty position
+        // differences[i] == 1  ==> extra seat in that position
+        // differences[i] == -1 ==> extra student without a seat in that position
+        
+
+        int moves = 0;
+        int unmatched = 0;
+
+        for(int i=1; i<m+1; i++) {
+            unmatched += differences[i];
+            moves = moves + abs(unmatched);
+        }
+
+        return moves;
     }
 };
