0df65725d843fc0af2bf412e74c7cb45ddab6407
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-22 10:54:04 +0530

    Time: 10 ms (54.67%) | Memory: 16.7 MB (15.17%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        // Better Solution
+        // T.C: O(2*n*m)
+        // S.C: O(n + m)
+
         int n = matrix.size();
         int m = matrix[0].size();
 
         vector<int> row(n, 0);
         vector<int> col(m, 0);
 
+        // Mark the rows/cols of zeros
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(matrix[i][j] == 0) {
@@ -16,6 +21,8 @@ public:
             }
         }
 
+        // Set an element = 0, if it's row/col is marked, i.e.
+        // it belongs to the row or column of a 0
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(row[i] == 1 || col[j] == 1) {
