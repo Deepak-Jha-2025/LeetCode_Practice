cd00c0e5cc94aed36f7fdf2a1dc9340a9024008b
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-23 19:56:00 +0530

    Time: 4 ms (38.50%) | Memory: 8.6 MB (61.06%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -8,8 +8,8 @@ public:
         // Create an ext matrix to store each element in its desired place 
         // and return the ans
         int n = matrix.size();
-        vector<vector<int>> ans(n, vector<int>(n));    // n x n matrix created, uninitialized
-        // vector<vector<int>> ans(n, vector<int>(n, 0)); // n x n matrix created, initialized with 0
+        // vector<vector<int>> ans(n, vector<int>(n));    // n x n matrix created, uninitialized
+        vector<vector<int>> ans(n, vector<int>(n, 0)); // n x n matrix created, initialized with 0
 
         // Iterate the matrix and map the elements
         for(int i=0; i<n; i++) {
@@ -18,7 +18,7 @@ public:
             }
         }
 
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n; i++) { // This extra O(n^2) b/z of the return type of the function
             for(int j=0; j<n; j++) {
                 matrix[i][j] = ans[i][j];
             }
