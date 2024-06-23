15366c0bfb9db1270b7fc6884eb3896991eee306
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-23 19:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (22.29%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // Brute Force
+        // T.C: O(n^2)
+        // S.C: O(n^2)
+
+        // Create an ext matrix to store each element in its desired place 
+        // and return the ans
+        int n = matrix.size();
+        vector<vector<int>> ans(n, vector<int>(n));    // n x n matrix created, uninitialized
+        // vector<vector<int>> ans(n, vector<int>(n, 0)); // n x n matrix created, initialized with 0
+
+        // Iterate the matrix and map the elements
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                ans[j][(n-1)-i] = matrix[i][j];
+            }
+        }
+
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+
+        matrix = ans;
+    }
+};
