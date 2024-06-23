fb28e43b22c7050913aeec777f0033164da36ad8
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-23 22:19:26 +0530

    Time: 4 ms (38.50%) | Memory: 8.7 MB (22.29%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -8,6 +8,7 @@ public:
         int n = matrix.size();
 
         // Step 1: Take transpose
+        // O(N*(N-1)/2)
         for(int i=0; i<n-1; i++) {
             for(int j=i+1; j<n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
@@ -15,6 +16,7 @@ public:
         }
 
         // Step 2: Reverse every row
+        // O(N * N/2)
         for(int i=0; i<n; i++) {
             // Every row is a vector in itself, i.e. matrix[i] is a vector
             reverse(matrix[i].begin(), matrix[i].end());
