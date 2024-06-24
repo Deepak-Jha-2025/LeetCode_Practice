9a06a3ffcbba77d8cd92322992f0c36b7789e851
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-25 03:09:58 +0530

    Time: 14 ms (23.14%) | Memory: 13.9 MB (18.05%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -7,10 +7,14 @@ public:
         int n = mat.size();
         int diagSum = 0;
         for(int i=0; i<n; i++) {
-            diagSum += mat[i][i];
+            diagSum += (mat[i][i] + mat[i][n-1-i]);
 
-            if(i != ((n-1)-i)) {
-                diagSum += mat[i][n-1-i];
+            if(i == (n-1-i)) {
+                // Same sum of centre ele(common to both diagonals) has been added twice
+                // so remove once (to compensate that extra addition)
+
+                // diagSum -= mat[i][n-1-i];
+                diagSum -= mat[i][i];
             }
         }
 
