4c3840f97c33205c381010160605a7ec472a5e9b
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-26 00:49:20 +0530

    Time: 12 ms (49.66%) | Memory: 12.8 MB (78.38%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        // Somewhat optimized brute/improved Brute
-        // T.C: O(n*m)
-        // S.C: O(1)
+        // Optimal 
+        // T.C: O(m+n) in worst case, O(m) or O(n) in best case
+        // s.C: O(1)
+
+        // start from top right(for row wise traversal) or bottom left(for col wise traversal)
+        // To understand why to start from there and also the time(dry run 3 test cases, one is
+        // the example 1 from the ques itself, and the other 2 cases of all +ves and all -ves)
+
+        // Idea is that it's sorted from left to right in decreasing order 
+        // so right to left it'll increase, so if a +ve found in the row iterating R to L
+        // that means uske piche ke saare bhi saare +ve hi honge surely, so skip that row
+
+        // Similarly for col, it is sorted in decreasing order from top to bottom, so while
+        // iterating from top to bottom, if a -ve is found, that means kyuki niche ki taraf
+        // values ghat rahi hain, aur abhi -ve mila hain, toh iske niche wale +ve thode honge, 
+        // -ve hi honge woh bhi, to remaining col from current index is the count, move to next col
 
         int m = grid.size();
         int n = grid[0].size();
-        
-        // sorted in decreasing order
-        // negatives are found/concentrated towards right and bottom portion
-        // so start looking from there and stop when a positive found, b/z 
-        // after that only +ves will be there, so no use iterating for that row
 
         int negCnt = 0;
-        for(int i=m-1; i>=0; i--) {
-            for(int j=n-1; j>=0; j--) {
-                if(grid[i][j] < 0) {
-                    negCnt++;
-                }
+        int row = 0;   // to keep track of row no.
+        int col = n-1; // to keep track of col no.
 
-                if(grid[i][j] > 0) {
-                    continue;
-                }
+        while(row < m && col >= 0) {
+
+            if(grid[row][col] < 0) {
+                // entire remaining col will also be -ves only, so count directly from idx
+                // assume hypothetically, before row idx, only +ves were there, and row is 
+                // the idx, where the 1st -ve of the col has occured, so row = no. of +ves so
+                // far in the jth col, so:
+                // no. of -ves = column size - row = m - row
+
+                negCnt += (m - row);
+                col--; // row is still fixed
+            }
+            else {
+                // all elements before this in prev cols will also be +ve 
+                // so no need to further check in this row and move to next row
+
+                row++; // col is still fixed
             }
         }
 
