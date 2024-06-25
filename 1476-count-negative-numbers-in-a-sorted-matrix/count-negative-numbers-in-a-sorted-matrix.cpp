377d25bf42fd81a060c002e55e9a2cdcee3fddc5
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-25 23:56:44 +0530

    Time: 10 ms (72.58%) | Memory: 12.9 MB (44.43%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        // Brute force
-        // T.C: O(m*n)
+        // Somewhat optimized brute/improved Brute
+        // T.C: O(n*m)
         // S.C: O(1)
 
         int m = grid.size();
         int n = grid[0].size();
-        int negCnt = 0;
+        
+        // sorted in decreasing order
+        // negatives are found/concentrated towards right and bottom portion
+        // so start looking from there and stop when a positive found, b/z 
+        // after that only +ves will be there, so no use iterating for that row
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
+        int negCnt = 0;
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
                 if(grid[i][j] < 0) {
                     negCnt++;
+                }
+
+                if(grid[i][j] > 0) {
+                    continue;
                 }
             }
         }
