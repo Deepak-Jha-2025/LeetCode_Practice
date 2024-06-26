0c2a35f6aa4ceea495ebf12d4d9c69b43a4f6a73
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-26 22:40:57 +0530

    Time: 75 ms (92.28%) | Memory: 38.2 MB (89.66%) - LeetSync

---
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        // Brute force - using bitmask array
+        int n = matrix.size();
+        vector<int> bitmask(n+1, 0); // O(n)
+
+        // Checking along all rows --> O(n*(3n))
+        for(int i=0; i<n; i++) {
+            // O(n)
+            for(int j=0; j<n; j++) {
+                bitmask[matrix[i][j]] = 1; // mark its presence
+            }
+
+            // Iterate in the bitmask to see if all elements from 1 to n where present in the row
+            // O(n)
+            for(int k=1; k<=n; k++) {
+                if(bitmask[k] == 0) {
+                    return false;
+                }
+            }
+
+            // Check for current row done, reset the bitmask to use for next row = O(n)
+            fill(bitmask.begin(), bitmask.end(), 0); // or simply use loop
+        }
+
+        // Checking along all cols --> O(n*(3n))
+        for(int j=0; j<n; j++) {
+            // O(n)
+            for(int i=0; i<n; i++) {
+                bitmask[matrix[i][j]] = 1; // mark its presence
+            }
+
+            // Iterate in the bitmask to see if all elements from 1 to n where present in the row
+            // O(n)
+            for(int k=1; k<=n; k++) {
+                if(bitmask[k] == 0) {
+                    return false;
+                }
+            }
+
+            // Check for current row done, reset the bitmask to use for next row = O(n)
+            fill(bitmask.begin(), bitmask.end(), 0); // or simply use loop
+        }
+
+        return true;
+    }
+};
