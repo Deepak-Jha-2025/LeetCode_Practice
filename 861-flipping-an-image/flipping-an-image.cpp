5e5bfa8b58956569e36e49b3b8347939ae14408b
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-26 15:19:06 +0530

    Time: 3 ms (72.42%) | Memory: 11.2 MB (44.17%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.cpp
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -3,22 +3,27 @@ public:
     void reverse(vector<int>& v) {
         int n = v.size();
 
-        // 2 ptrs
-        int l = 0, r = n-1;
+        // // 2 ptrs
+        // int l = 0, r = n-1;
 
-        while(l <= r) {
-            int temp = v[l];
-            v[l] = v[r] ^ 1;
-            v[r] = temp ^ 1;
-            l++, r--;
+        // while(l <= r) {
+        //     int temp = v[l];
+        //     v[l] = v[r] ^ 1;
+        //     v[r] = temp ^ 1;
+        //     l++, r--;
+        // }
+
+        // 1 ptr or idx
+        for(int i=0; i<n/2; i++) {
+            int temp = v[i];
+            v[i] = v[n-1-i] ^ 1; // to flip a bit, xor it with 1 (0 ^ 1 = 1 && 1 ^ 1 = 0)
+            v[n-1-i] = temp ^ 1;
         }
 
-        // // 1 ptr or idx
-        // for(int i=0; i<=n/2; i++) {
-        //     int temp = v[i];
-        //     v[i] = v[n-1-i] ^ 1; // to flip a bit, xor it with 1 (0 ^ 1 = 1 && 1 ^ 1 = 0)
-        //     v[n-1-i] = temp ^ 1;
-        // }
+        if(n % 2 != 0) {
+            // n is odd and a centre element exists that is still left out to flip
+            v[n/2] ^= 1;
+        }
     }
 
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
