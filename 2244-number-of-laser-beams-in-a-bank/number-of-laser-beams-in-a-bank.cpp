f9ec411f4419e8a2325a1b49205112014eecaa00
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-28 01:59:45 +0530

    Time: 84 ms (78.69%) | Memory: 26.4 MB (93.84%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -7,9 +7,9 @@ public:
         // how does this thought come up in the 1st place?
         /* Look at the better solution, while we iterate in the hash table
            to calculate the total number of beams, everytime we just need 2 values,
-           the prevRow/upperFloor and the currentRow/lowerFloor, once we calc a product
-           from the 2, then the currentRow becomes the prevRow and we go on to next for 
-           the next currentRow/lowerFloor, there is no further need of the used prevRow(s)
+           the prevDeviceCount/upperFloor and the currentDeviceCount/lowerFloor, once we calc a product
+           from the 2, then the currentDeviceCount becomes the prevDeviceCount and we go on to next for 
+           the next currentDeviceCount/lowerFloor, there is no further need of the used prevDeviceCount(s)
            
            This gives us the idea, that there was never a need in the 1st place to store 
            all the counts, just the current and its prev count are sufficient at each stage */
@@ -22,25 +22,25 @@ public:
         int m = bank.size();
         int n = bank[0].size();
 
-        int prevRow = 0;    // no. of lasers on upper floor/ previous floor/row
-        int currentRow = 0; // no. of lasers on lower floor/ current floor/row
+        int prevDeviceCount = 0;    // no. of lasers on upper floor/ previous floor/row
+        int currentDeviceCount = 0; // no. of lasers on lower floor/ current floor/row
 
         int totalLasers = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(bank[i][j] == '1') {
-                    currentRow++;
+                    currentDeviceCount++;
                 }
             }
 
-            if(currentRow != 0) { // means current row has devices
+            if(currentDeviceCount != 0) { // means current row has devices
 
-                // in 1st iteration, total = 0, b/z there is no prevRow yet, so where will the lasers come from
-                totalLasers += (prevRow * currentRow); 
+                // in 1st iteration, total = 0, b/z there is no prevDeviceCount yet, so where will the lasers come from
+                totalLasers += (prevDeviceCount * currentDeviceCount); 
 
-                prevRow = currentRow; // Now, this currentRow will become the prevRow for the upcoming currentRow
+                prevDeviceCount = currentDeviceCount; // Now, this currentDeviceCount will become the prevDeviceCount for the upcoming currentDeviceCount
 
-                currentRow = 0; // reset currentRow to store the no. of devices for the next upcoming row that has got some devices
+                currentDeviceCount = 0; // reset currentDeviceCount to store the no. of devices for the next upcoming row that has got some devices
             }
         }
 
