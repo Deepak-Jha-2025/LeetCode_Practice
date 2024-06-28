fbe6ef8b5aa0fdce0798dee013a27ded5ee8ba41
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-28 23:30:51 +0530

    Time: 17 ms (38.60%) | Memory: 14.8 MB (5.74%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -27,34 +27,39 @@ public:
         // satisfies the given min-max constraints, by everytime 
         // finding the min and max of each row and col)) ==> T.C: O((m*n)^2)
 
-        // Better Solution (using hashtables to keep track of min for each row and max for each col)
+        // Better Solution (using sets to keep track of min for each row and max for each col)
         // T.C: O(3mn)
         // S.C: O(m+n)
 
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<int> row_minValues(m); // to store the minimum val of each row 
-        vector<int> col_maxValues(n); // to store the maximum val of each col 
+        unordered_set<int> row_minValues; // to store the minimum val of each row 
+        unordered_set<int> col_maxValues; // to store the maximum val of each col 
 
         // Row minimums O(m*n)
         for(int i=0; i<m; i++) {
-            row_minValues[i] = findRowMin(matrix, i);
+            row_minValues.insert(findRowMin(matrix, i));
         }
 
         // Col maximums O(m*n)
         for(int j=0; j<n; j++) {
-            col_maxValues[j] = findColMax(matrix, j);
+            col_maxValues.insert(findColMax(matrix, j));
         }
 
-        // Find lucky no.s (should be present in both the arrays)
+        // Now that we are using sets, so no need to iterate in matrix again (O(m*n)) 
+        // or no need to linear search every value of one array in the other (O(m*n))
+
+        // As unordered_set takes O(1) time in avg and best case to search, so using .find() 
+        // function in any of the 2 sets (either the size m set or the size n set) would take
+        // O(m) or O(n) time
+
+        // Find lucky no.s (should be present in both the sets)
         vector<int> luckyNums;
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                
-                if(matrix[i][j] == row_minValues[i] && matrix[i][j] == col_maxValues[j]) {
-                    luckyNums.push_back(matrix[i][j]);
-                }
+        unordered_set<int>::iterator it;
+        for(it = row_minValues.begin(); it != row_minValues.end(); it++) {
+            if(col_maxValues.find(*it) != col_maxValues.end()) {
+                luckyNums.push_back(*it);
             }
         }
 
