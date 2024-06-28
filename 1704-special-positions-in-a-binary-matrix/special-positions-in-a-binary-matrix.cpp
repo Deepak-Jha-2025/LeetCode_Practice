084e4016ad0b18502db9536de6d3dd46fc245310
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-06-29 01:49:23 +0530

    Time: 15 ms (58.90%) | Memory: 15.4 MB (78.42%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,48 +1,61 @@
 class Solution {
 public:
-    bool checkRow(vector<vector<int>>& mat, int row) {
-        
+    int countRowOnes(vector<vector<int>>& mat, int& rowOnesCount, int row) {
+
+        int colIdx = 0;
         int n = mat[row].size();
-        int oneCount = 0;
         for(int j=0; j<n; j++) {
             if(mat[row][j] == 1) {
-                oneCount++;
+                colIdx = j;
+                rowOnesCount++;
             }
         }
-
-        return (oneCount == 1); // validates row conditions, all others in its row are 0
+        return colIdx;
     }
 
-    bool checkCol(vector<vector<int>>& mat, int col) {
-        
+    int countColOnes(vector<vector<int>>& mat, int col) {
+
+        int cnt = 0;
         int m = mat.size();
-        int oneCount = 0;
         for(int i=0; i<m; i++) {
             if(mat[i][col] == 1) {
-                oneCount++;
+                cnt++;
             }
         }
-
-        return (oneCount == 1); // validates row conditions, all others in its row are 0
+        return cnt;
     }
 
     int numSpecial(vector<vector<int>>& mat) {
-        // Brute force
-        // T.C: O(m*n*(m+n))
-        // S.C: O(1)
+        // Better solutin (uses hash tables for rows/cols)
+        // Similar to approach 2 or editorial (just a bit diff. version)
+
+        // T.C: O(m*n) + O(m*m) = O(m*(n+m))
+        // S.C: O(m)
 
         int m = mat.size();
         int n = mat[0].size();
 
+        // Just for row is enough, this would 1st check if the row i has only a single 1 or not
+        // If it does, then the col j of that 1, will be stored in this hash table, as the pos(i,j)
+        // can potentially be a special position only if the col j also has just a single 1, which 
+        // we will check after filling the row vector.
+        vector<int> row(m);
+
         int specialPos = 0;
         for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-
-                if(mat[i][j] == 1) {
-                    // criteria 1st fulfilled, potential possible special pos
-                    if(checkRow(mat, i) && checkCol(mat, j)) {
-                        specialPos++;
-                    }
+            
+            int rowOnesCount = 0;
+            int j = countRowOnes(mat, rowOnesCount, i); // j = col where the single '1' of the row is present
+            if(rowOnesCount == 1) {
+                // mat[i][j] == 1 (validated)
+                // all other elements in ith row = 0 (validated)
+                // So, (i, j) is a potential special postition (it may or may not be), 
+                // depends on the col (if all other elements in jth col = 0 <==> oneCount == 1
+                // for that col also)
+                
+                if(countColOnes(mat, j) == 1) {
+                    // i, j == special position
+                    specialPos++;
                 }
             }
         }
