411af30d7ccbc5a7a74931daf460cb6a8f918b69
Deepak-Jha-2025 <129058395+Deepak-Jha-2025@users.noreply.github.com>
2024-07-01 18:58:18 +0530

    Time: 3 ms (41.54%) | Memory: 7.5 MB (87.55%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long nCr(int n, int r) {
         long long res = 1;
-        for(int i=0; i<r; i++) {
+        for(long long i=0; i<r; i++) {
             res = res * (n-i);
             res = res / (i+1);
         }
@@ -11,16 +11,17 @@ public:
 
     vector<int> getRow(int rowIndex) {
         // Brute Force
-        // T.C:
+        // T.C: O(n * r) , n = rowIdx + 1, r = colIdx
         // S.C: O(1)
 
         // No. of elements in a row = rowIndex(0-based) + 1
         int n = rowIndex + 1;
-        vector<int> row(n, 0);
+        vector<int> row;
 
         for(int colIndex=0; colIndex < n; colIndex++) 
         {
-            row[colIndex] = nCr(rowIndex, colIndex);
+            // row[colIndex] = nCr(rowIndex, colIndex);
+            row.push_back(nCr(rowIndex, colIndex));
         }
 
         return row;
